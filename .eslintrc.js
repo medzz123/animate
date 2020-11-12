00da4f2952622a4884c9d7763790e984065c81f7
Mahedi Hasan <mahedi.hs1997@gmail.com>
2020-11-12 21:54:34 +0000

    Adding eslint to project

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ["simple-import-sort"],
+    parserOptions: {
+      ecmaFeatures: { jsx: true },
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:jsx-a11y/recommended',
+  
+      // Prettier plugin and recommended rules
+      'prettier/@typescript-eslint',
+      'plugin:prettier/recommended',
+    ],
+    rules: {
+      // Include .prettierrc.js rules
+      'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+  
+      // suppress jsx in scope
+      "react/react-in-jsx-scope": "off",
+      
+      'react/prop-types': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'jsx-a11y/anchor-is-valid': 'off',
+      "simple-import-sort/sort": "error"
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { ThemeProvider } from "styled-components";
-import { theme } from "@theme/theme";
 import { GlobalStyle } from "@theme/globalStyles";
+import { theme } from "@theme/theme";
+import { ThemeProvider } from "styled-components";
 
 const App = ({ Component, pageProps }) => {
   return (
