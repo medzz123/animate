57992ee643b47c2691c53071c185a2bb3f8cd8e6
Mahedi Hasan <mahedi.hs1997@gmail.com>
2020-11-15 16:31:51 +0000

    fix(structure): Fix pages structure

---
+++ src/pages/Animate/Animate.styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: calc(100vh - 51px);
+  width: 100vw;
+`;
+
+export const MainWrapper = styled.div`
+  display: flex;
+  height: calc(100vh - 51px - 250px);
+`;
--- src/pages/Animate/Animate.tsx
+++ src/pages/Animate/Animate.tsx
@@ -4,17 +4,8 @@ import ElementBox from '@components/ElementBox';
 import PropertiesBox from '@components/PropertiesBox';
 import { NextPage } from 'next';
 import React from 'react';
-import styled from 'styled-components';
 
-const Container = styled.div`
-  height: calc(100vh - 51px);
-  width: 100vw;
-`;
-
-const MainWrapper = styled.div`
-  display: flex;
-  height: calc(100vh - 51px - 250px);
-`;
+import { Container, MainWrapper } from './Animate.styles';
 
 const Animate: NextPage = () => {
   return (
