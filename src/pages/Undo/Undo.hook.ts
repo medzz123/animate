0d9b4a0ccdbb4f67f4dbc83d5f89b7c39dfc1f4b
Mahedi Hasan <mahedi.hs1997@gmail.com>
2021-03-22 20:23:44 +0000

    feat(component): CA-33 Undo prototype

---
+++ src/pages/Undo/Undo.hook.ts
@@ -0,0 +1,56 @@
+import { useCallback, useState } from 'react';
+
+export function useUndo(initialPresent) {
+  const [state, setState] = useState({
+    past: [],
+    present: initialPresent,
+    future: [],
+  });
+  const canUndo = state.past.length !== 0;
+  const canRedo = state.future.length !== 0;
+  const undo = useCallback(() => {
+    setState((currentState) => {
+      const { past, present, future } = currentState;
+      if (past.length === 0) return currentState;
+      const previous = past[past.length - 1];
+      const newPast = past.slice(0, past.length - 1);
+      return {
+        past: newPast,
+        present: previous,
+        future: [present, ...future],
+      };
+    });
+  }, []);
+  const redo = useCallback(() => {
+    setState((currentState) => {
+      const { past, present, future } = currentState;
+      if (future.length === 0) return currentState;
+      const next = future[0];
+      const newFuture = future.slice(1);
+      return {
+        past: [...past, present],
+        present: next,
+        future: newFuture,
+      };
+    });
+  }, []);
+  const set = useCallback((newPresent) => {
+    setState((currentState) => {
+      const { present, past } = currentState;
+      if (newPresent === present) return currentState;
+      return {
+        past: [...past, present],
+        present: newPresent,
+        future: [],
+      };
+    });
+  }, []);
+  const reset = useCallback((newPresent) => {
+    setState(() => ({
+      past: [],
+      present: newPresent,
+      future: [],
+    }));
+  }, []);
+  return { state, handlers: { set, reset, undo, redo, canUndo, canRedo } };
+}
---
+++ src/pages/Undo/Undo.tsx
@@ -0,0 +1,34 @@
+import { NextPage } from 'next';
+import React from 'react';
+
+import { useUndo } from './Undo.hook';
+import { Container, Content } from './Undo.styles';
+
+const Undo: NextPage = () => {
+  const { state, handlers } = useUndo([1, 2, 3, 4, 5]);
+
+  return (
+    <Container>
+      <Content>
+        <p>Hello, this page us Undo</p>
+        <button
+          onClick={() => {
+            handlers.set([...state.present, Math.random()]);
+          }}
+        >
+          add item to list
+        </button>
+        <button onClick={handlers.undo}>go back</button>
+        <button onClick={handlers.redo}>go forward</button>
+
+        {state.present.map((m) => (
+          <div key={m}>
+            <p>{m}</p>
+          </div>
+        ))}
+      </Content>
+    </Container>
+  );
+};
+
+export default Undo;
