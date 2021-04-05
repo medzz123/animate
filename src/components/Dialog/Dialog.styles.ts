dce6c2661791873b8d5eb70eda4247fee9f0251e
Mahedi Hasan <mahedi.hs1997@gmail.com>
2021-04-05 23:49:44 +0100

    feat(page): fix dialog overflow bug

--- src/components/Dialog/Dialog.styles.ts
+++ src/components/Dialog/Dialog.styles.ts
@@ -19,18 +19,18 @@ export const StyledOverlay = styled(Overlay)`
 
 export const StyledContent = styled(Content)`
   position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  min-width: 200px;
-  width: calc(100vw - 48px);
+  top: ${tokens.sizes.half};
+  left: ${tokens.sizes.half};
+  transform: translate(-${tokens.sizes.half}, -${tokens.sizes.half});
+  min-width: ${tokens.sizes['3xs']};
+  width: calc(100vw - ${tokens.sizes[48]});
   max-height: 85vh;
-  padding: 20px;
-  margin-top: -5vh;
-  border-radius: 6px;
+  padding: ${tokens.sizes[12]};
+  border-radius: ${tokens.sizes[6]};
   padding: ${tokens.sizes[24]};
   background-color: ${(p) => p.theme.background};
   color: ${(p) => p.theme.black};
+  overflow-y: scroll;
 
   ${mq(768)} {
     max-width: fit-content;
